2fae37e70f77b3e7ceafa545f079d5522333368b
Andreas Eckleder <andreas@eckleder.de>
2023-09-16 10:17:38 +0200

    Make the esp8266 remote receiver compile for RP2040.

--- esphome/components/remote_receiver/remote_receiver_esp8266.cpp
+++ esphome/components/remote_receiver/remote_receiver_esp8266.cpp
@@ -3,7 +3,7 @@
 #include "esphome/core/log.h"
 #include "esphome/core/helpers.h"
 
-#ifdef USE_ESP8266
+#if defined(USE_ESP8266) || defined(USE_RP2040)
 
 namespace esphome {
 namespace remote_receiver {
