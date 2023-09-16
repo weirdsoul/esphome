a69db530642148c6c426ff406489dffd6d4d8fe9
Andreas Eckleder <andreas@eckleder.de>
2023-09-16 12:44:51 +0200

    Make remote receiver compile and run on RP2040 platform

--- esphome/components/remote_transmitter/remote_transmitter_esp8266.cpp
+++ esphome/components/remote_transmitter/remote_transmitter_esp8266.cpp
@@ -2,7 +2,7 @@
 #include "esphome/core/log.h"
 #include "esphome/core/application.h"
 
-#ifdef USE_ESP8266
+#if defined(USE_ESP8266) || defined(USE_RP2040)
 
 namespace esphome {
 namespace remote_transmitter {
